ce5652adca79bc806b2518954ccddc149c91bd81
Aryan Goswami <agoswami1238@gmail.com>
2023-05-23 20:51:48 -0400

    got down basic conversations with solid mimicking although, whenever it's asked for help it acts more like base chatGPT

---
+++ him.py
@@ -0,0 +1,35 @@
+import os
+import openai
+from config import OPENAI_API_KEY
+from conversation import MESSAGES
+
+openai.api_key = OPENAI_API_KEY
+
+openai.ChatCompletion.create(
+            model='gpt-3.5-turbo',
+            messages=MESSAGES
+)
+
+def run_chat():
+    history = MESSAGES
+
+    while True:
+        user_msg = input("me: ")
+        history.append({"role": "user", "content": user_msg})
+        response = openai.ChatCompletion.create(
+            model='gpt-3.5-turbo',
+            messages=MESSAGES
+        )
+        chatbot_reply = response['choices'][0]['message']['content']
+        print("me AI:", chatbot_reply)
+        history.append({"role": "assistant", "content": chatbot_reply})
+
+        if end(user_msg):
+            break
+
+def end(msg):
+    if "bye" in msg.lower():
+        return True
+    return False
+
+run_chat()
